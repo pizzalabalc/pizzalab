2d7d0f1d92af376ac786990a25e8ca3523377efb
pizzalab <55190933+pizzalabalc@users.noreply.github.com>
2026-04-01 02:51:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const observer = new IntersectionObserver(entries => {
       entry.target.classList.add('visible');
     }
   });
-},{ threshold: 0.2 });
+}, { 
+  threshold: 0.05,        /* Se activa mucho antes (al asomar un 5%) */
+  rootMargin: "0px 0px -20px 0px" /* Anticipa la carga para que no parezca que tarda */
+});
 
 faders.forEach(el => observer.observe(el));
